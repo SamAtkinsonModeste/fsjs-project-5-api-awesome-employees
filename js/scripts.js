b825721d4980b40ba85ffbeab0b60bf702282211
Sam Atkinson Modeste <sammodeste@yahoo.com>
2026-07-06 17:33:51 +0100

    feat: Add dynamic search UI
    Created the search input, clear button, label and datalist, and populated the option elements with the first and last names of the employees data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const galleryDiv = document.querySelector("#gallery");
+const searchDiv = document.querySelector(".search-container");
 
 const randomUsersUrl =
   "https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=gb";
@@ -27,6 +28,7 @@ async function fetchEmployeesData() {
   try {
     const data = await fetchData(randomUsersUrl);
     createEmployeeCards(data.results);
+    createSearchUI(data.results);
   } catch (error) {
     console.error(error);
   }
@@ -67,5 +69,28 @@ function createEmployeeCards(employees) {
       `;
 
     galleryDiv.insertAdjacentHTML("beforeend", employeeHTML);
+  });
+}
+
+function createSearchUI(employees) {
+  const searchInput = `
+   <input autocomplete="off" type="text" name="search" id="search-input" list="">
+                <button id="search-close" title="Clear Search Field & Reset Employees">X</button>
+                <label for="search-input">Search Employees</label>`;
+
+  searchDiv.insertAdjacentHTML("afterbegin", searchInput);
+
+  const dataList = `<datalist id="data-names"></datalist>`;
+  searchDiv.insertAdjacentHTML("beforeend", dataList);
+
+  const dataListElement = document.querySelector("#data-names");
+
+  employees.forEach((employee) => {
+    const firstName = employee.name.first;
+    const lastName = employee.name.last;
+    const option = document.createElement("option");
+    option.value = `${firstName} ${lastName}`;
+    option.textContent = `${firstName} ${lastName}`;
+    dataListElement.appendChild(option);
   });
 }
